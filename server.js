96c66b592632a44cad51ba46c29147a10834a79c
squall2046 <squall2046@hotmail.com>
2019-04-19 14:49:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ mongoose.connect(mongoURL, { useNewUrlParser: true })
 
 // Passport Config
 require('./config/passport')(passport);
-require('./config/auth');
 
 // Express session
 app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }));
